b81d893ca47e49ad4155eeced6e4be5078928178
karan <karan@ubuntu.com>
2019-03-03 02:41:30 -0800

    Added DTC handle

--- DataFetch.py
+++ DataFetch.py
@@ -1,7 +1,7 @@
 import obd
 import time
 import serial
-from OBDCodes import Codes
+#from OBDCodes import Codes
 
 class FetchObd:
 	@staticmethod
@@ -9,7 +9,7 @@ class FetchObd:
 		obd.logger.setLevel(obd.logging.DEBUG)   # debug
 		ports = obd.scan_serial()                # return list of valid USB or RF ports
 		print(ports)                             # ['/dev/ttyUSB0', '/dev/ttyUSB1']
-		connection = obd.OBD('COM10', baudrate=38400, fast=False)
+		connection = obd.OBD(ports[0], baudrate=38400, fast=False)
 		# cmd = obd.commands.PIDS_A
 		# response = connection.query(cmd)
 		# accepted_protocols=[]
@@ -18,35 +18,37 @@ class FetchObd:
 		# 	if response.value[pid] == True:
 		# 		accepted_protocols.append(hex(pid))
 		return connection
-
 	@staticmethod
-	def fetch_data():
-		# connection = FetchObd.fetch_protocols()
+	def fetch_data(connection):
+		#connection = FetchObd.fetch_protocols()
 		dict = {}
-		# cmd = obd.commands.COOLANT_TEMP
-		# response = connection.query(cmd)
-		# print("COOLANT_TEMP : ",response.value)
-		# dict['COOLANT_TEMP'] = response.value
-		dict['COOLANT_TEMP'] = 38
-		# cmd = obd.commands.SPEED
-		# response = connection.query(cmd)
-		# print("SPEED : ",response.value)
-		# dict['SPEED'] = response.value
-		dict['SPEED'] = 0
-		# cmd = obd.commands.RPM
-		# response = connection.query(cmd)
-		# print("RPM : ",response.value)
-		# dict['RPM'] = response.value
-		dict['RPM'] = 100
-		# command_list = []
-		# for pid in protocol_list:
-		# 	command = Codes.codes[pid]
-		# 	command_list.append(command)
-		# 	print(command)
-		# 	cmd = obd.commands.STATUS
-		# 	response = connection.query(cmd)
-			
-		# 	dict[command] = response.value
-		return dict	
-
-
+		cmd = obd.commands.COOLANT_TEMP
+		response = connection.query(cmd)
+		if not response.is_null():
+			print("COOLANT_TEMP : ",response.value)
+			dict['COOLANT_TEMP'] = response.value.magnitude
+		# dict['COOLANT_TEMP'] = 38
+		cmd = obd.commands.SPEED
+		response = connection.query(cmd)
+		if not response.is_null():
+			print("SPEED : ",response.value)
+			dict['SPEED'] = response.value.magnitude
+		# dict['SPEED'] = 0
+		cmd = obd.commands.RPM
+		response = connection.query(cmd)
+		if not response.is_null():
+			print("RPM : ",response.value)
+			dict['RPM'] = response.value.magnitude
+		# dict['RPM'] = 100
+		cmd = obd.commands.STATUS
+		response = connection.query(cmd)
+		if not response.is_null():
+			print("STATUS : ",response.value.MIL)
+			print("Status : ",response.value.DTC_count)
+			dict['EngineStatus'] = response.value.MIL
+			if response.value.MIL == True:
+				cmd = obd.commands.GET_DTC
+				response = connection.query(cmd)
+				print("DTC : ",response.value)
+				dict['DTC'] = response.value
+		return dict
--- main.py
+++ main.py
@@ -1,35 +1,33 @@
 from DataFetch import FetchObd
-from GPSFetch import FetchGps
+# from GPSFetch import FetchGps
 from CloudStorage import CloudStore
-from LocalStorage import Storage
+# from LocalStorage import Storage
 import json
 import datetime
 import time
 class FleetData:
 	@staticmethod
 	def fleet_data():
-		# protocols = FetchObd.fetch_protocols()	#Not used anywhere
+		conn_obd = FetchObd.fetch_protocols()	
 		now = datetime.datetime.now()
 		conn_cloud = CloudStore.cloud_connect()
-		conn_local = Storage.connection()
-		Storage.create_table(conn_local)
-		# while True:
-		# print(protocols)	
-		dict = {}
-		time.sleep(5)
-		dict = FetchObd.fetch_data()
-		dict['date'] = now.strftime("%Y-%m-%d")
-		dict['time'] = now.strftime("%H:%M")
-		dict['latitude'],dict['longitude'] = 0,0
-		# dict['latitude'],dict['longitude'] = FetchGps.gps_info()
-		dict1 = json.dumps(dict)
-		dict2 = json.loads(dict1)
-		print(dict2)
-		Storage.insert_data(conn_local,dict2)
-		CloudStore.cloud_insert(conn_cloud,dict2)
-		Storage.display_data(conn_local)
-		Storage.close_connection(conn_local)
+		# conn_local = Storage.connection()
+		# Storage.create_table(conn_cloud)
+		while True:
+			# print(protocols)	
+			dict = {}
+			time.sleep(5)
+			dict = FetchObd.fetch_data(conn_obd)
+			dict['date'] = now.strftime("%Y-%m-%d")
+			dict['time'] = now.strftime("%H:%M")
+			dict['latitude'],dict['longitude'] = 0,0
+			# dict['latitude'],dict['longitude'] = FetchGps.gps_info()
+			dict1 = json.dumps(dict)
+			dict2 = json.loads(dict1)
+			print(dict2)
+			# Storage.insert_data(conn_local,dict2)
+			# CloudStore.cloud_insert(conn_cloud,dict2)
+			# Storage.close_connection(conn_local)
 
 if __name__ == '__main__':
 	FleetData.fleet_data()
-
